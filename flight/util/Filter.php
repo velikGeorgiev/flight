d612d45c739c161f2a528817e22018e1d0e8ffa6
Velik Georgiev <velko_bg@abv.bg>
2015-10-28 20:17:54 +0100

    Create Filter.php

---
+++ flight/util/Filter.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Flight: An extensible micro-framework.
+ *
+ * @author Velik Georgiev Chelebiev
+ * @version 0.0.1
+ * @license MIT License
+ */
+namespace core;
+class Filter {
+    /**
+     * Applys htmlspecialchars and addslshed to the given parameter.
+     * If the parameter is an array walks throw it and apply the changes
+     * to all elements.
+     * 
+     * It can be used to filter $_POST or/and $_GET. Example:
+     * Filter::htmlChars($_POST);
+     * Filter::htmlChars($_GET);
+     *
+     * @param mixed $data Variable that must be "filtered" by using htmlspecialchars and addslashes
+     * @return mixed
+     */
+    public static function htmlChars(&$data) {
+        if(is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = self::htmlChars($value);
+            }
+        } else {
+            $data = htmlspecialchars(addslashes($data));
+        }   
+        
+        return $data;
+    }
+    
+    /**
+     * Casts the given parameter to integer.
+     * If array is passed walk the array and cast all
+     * the elements recursive.
+     * 
+     * @param mixed $data Variable to be cast to int
+     * @author Velik Georgiev Chelebiev <bulter2005@gmail.com>
+     * @return int|array
+     */
+    public static function castNumber(&$data) {
+        if(is_array($data)) {
+            foreach ($data as $key => $value) {
+                $data[$key] = self::castNumber($value);
+            }
+        } else {
+            $data = (int)$data;
+        }   
+        
+        return $data;
+    }
+}
+?>
